c455f6d549a2ed7f62b56eb712116c58977e8e92
Hamxah4U <hamxah4u@gmail.com>
2026-03-23 00:07:20 +0100

    first commit

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -1,3 +1,11 @@
+<!-- jQuery -->
 <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+
+<!-- Bootstrap -->
 <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+<!-- jQuery Easing (Required for SB Admin) -->
+<script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+
+<!-- SB Admin Script -->
 <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,4 +1,5 @@
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion">
+{{-- <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion"> --}}
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
         <div class="sidebar-brand-icon rotate-n-15">
@@ -23,11 +24,22 @@
         </a>
     </li>
 
-    <li class="nav-item {{ request()->routeIs('users') ? 'active' : '' }}">
-    <a class="nav-link" href="">
-        <i class="fas fa-fw fa-users"></i>
-        <span>Users</span>
-    </a>
-</li>
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+            aria-expanded="true" aria-controls="collapseTwo">
+            <i class="fas fa-fw fa-cog"></i>
+            <strong><span>Health Info</span></strong>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                {{-- <h6 class="collapse-header">Custom Components:</h6> --}}
+                <a class="collapse-item" href="{{ route('patients.create') }}">Add Patient</a>
+                <a class="collapse-item" href="{{ route('patients.index') }}">View Patients</a>
+                <a class="collapse-item" href="{{ route('appointments.index') }}">Manage Appointments</a>
+                <a class="collapse-item" href="{{ route('records.index') }}">Health Records</a>
+
+            </div>
+        </div>
+    </li>
 
 </ul>
